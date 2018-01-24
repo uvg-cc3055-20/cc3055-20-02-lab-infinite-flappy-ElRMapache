1a5001fed9799b35128a7de802fcdac7c8fd3440
ElRMapache <alo15470@uvg.edu.gt>
2018-01-24 10:10:11 -0600

    Lab2
    todo el lab 2

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -12,10 +12,24 @@ public class BirdScript : MonoBehaviour {
 	}
 	
 	void Update () {
-        if (Input.GetButtonDown("Jump")) {
-            rb.velocity = Vector2.zero;
-            rb.AddForce(Vector2.up * jumpForce);
+        if (GameController.instance.gameOver == false)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                rb.velocity = Vector2.zero;
+                rb.AddForce(Vector2.up * jumpForce);
+            }
         }
 	}
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameController.instance.score++;
+        print("El punteo es: " + GameController.instance.score);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       GameController.instance.gameOver = true;
+    }
 }
